f48c4463d12db5821403046156ec317dea986a31
Robert Lee <bobbydwise1@hotmail.com>
2019-09-19 16:45:47 -0700

    Add edge case handling.

--- scripts.js
+++ scripts.js
@@ -45,26 +45,41 @@ const findProduct = (yourArray) => {
     }
   )
   negative = sortDescending(negative)
-  console.log(negative)
   let positive = copied.filter(
     function(element) {
       return element >= 0
     }
   )
   console.log(positive)
-  if (negative.length >= 2) {
-    positive.push(-negative[0])
-    positive.push(-negative[1])
+  if ((negative.length >= 2)&&(positive.length >= 1)) {
+    let temp1 = negative[1]
+    negative = negative[0]*temp1
+  } else if ((negative.length == 0)&&(positive.length >= 1)) {
+    negative = 0
+  } else {
+    return negative[negative.length-1]*negative[negative.length-2]*negative[negative.length-3]
   }
+  // console.log(negative)
   positive = sortAscending(positive)
-  console.log(positive)
-  return positive[0]*positive[1]*positive[2]
+  // console.log(positive)
+  let first = positive[0]*positive[1]*positive[2]
+  let second = positive[0]*negative
+  if (first > second) {
+    return first
+  } else {
+    return second
+  }
 }
 
-let test1 = [-10,-10,5,2]
-console.log(findProduct(test1))
+// let test1 = [-10,-10,5,2]
+// console.log(findProduct(test1))
 
-let test2 = [-10,-8,-6,5,7,9,11]
+// let test2 = [-10,-8,-6,5,7,9,11]
+// console.log(findProduct(test2))
+
+// let test3 = [-10,-8,-6,-5,-7,-9,-11]
+// console.log(findProduct(test3))
+
 
 $(document).ready(function() {
 
