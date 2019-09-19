95df4f87dddb1d9f261860898ed8f32199f2e445
Robert Lee <bobbydwise1@hotmail.com>
2019-09-19 14:50:35 -0700

    Add functions.

--- scripts.js
+++ scripts.js
@@ -8,7 +8,36 @@ For example, if the list is [-10, -10, 5, 2], we should return 500, since that's
 You can assume the list has at least three integers.
 */
 
+const sortDescending = (yourArray) => {
+  let copied = yourArray.slice()
+  return copied.sort(function(a,b){
+      return a-b
+    }
+  )
+}
 
+const findProduct = (yourArray) => {
+  if (yourArray.length <= 2) {
+    return null
+  } else if (yourArray.length == 3) {
+    return yourArray[0]*yourArray[1]*yourArray[2];
+  }
+  let negative = yourArray.filter(
+    function(element) {
+      return element < 0
+    }
+  )
+  console.log(negative)
+  let positive = yourArray.filter(
+    function(element) {
+      return element >= 0
+    }
+  )
+  console.log(positive)
+}
+
+let test1 = [-10,-10,5,2]
+console.log(findProduct(test1))
 
 $(document).ready(function() {
 
